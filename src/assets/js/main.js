9ba6c6e1ef1cb5d6fb99956d97e0f06d026791d2
Ulguim <williamulguim2014@gmail.com>
2023-06-27 21:09:37 -0300

    :construction: Makes improvements

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,3 +9,24 @@ function handleTopnav() {
         x.className = "topNav";
     }
 }
+
+
+const showFileName = () => {
+    let button = document.getElementById('file');
+    document.getElementById('file').addEventListener('change', function(e) {
+        document.querySelector('#fileName').innerHTML = '';
+        if (e.target.files[0]) {
+          var fileNameElement = document.createElement('p');
+          fileNameElement.textContent = e.target.files[0].name;
+       
+          document.querySelector('#fileName').appendChild(fileNameElement);
+        }
+      });
+      
+    const input = document.getElementById('file');
+    const output = document.getElementById('file-name');
+
+    output.textContent = input.files[0].name;
+}
+
+
